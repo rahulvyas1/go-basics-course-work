27daa78004a0558a81ee63b013764296c7abcdf6
rahulvyas1 <rahul.class@gmail.com>
2019-10-26 13:23:08 +0530

    go routines

---
+++ channels-website-status-checker/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	links := []string{
+		"http://google.com",
+		"http://facebook.com",
+		"http://golang.org",
+		"http://stackoverflow.com",
+	}
+
+	c := make(chan string)
+
+	for _, link := range links {
+		checkLink(link, c)
+	}
+}
+
+func checkLink(link string, c chan string) {
+	_, err := http.Get(link)
+	if err != nil {
+		fmt.Println(link, "might be down!")
+		return
+	}
+	fmt.Println(link, "is up!")
+}
